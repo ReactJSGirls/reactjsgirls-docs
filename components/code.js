094b69d867f95d91f47c787863e7bbfd87f7c9bf
Sara Vieira <hey@iamsaravieira.com>
2018-09-23 21:43:42 +0200

    prettuer

--- components/code.js
+++ components/code.js
@@ -1,25 +1,25 @@
-import React from "react"
-import Highlight, { defaultProps } from "prism-react-renderer"
-import nightOwl from "prism-react-renderer/themes/nightOwl"
-import { CopyToClipboard } from "react-copy-to-clipboard"
-import { FaCopy } from "react-icons/fa"
+import React from 'react'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import nightOwl from 'prism-react-renderer/themes/nightOwl'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { FaCopy } from 'react-icons/fa'
 
 const styles = style => ({
   ...style,
   padding: 20,
-  position: "relative"
+  position: 'relative'
 })
 
 const buttonStyles = {
   border: 0,
   padding: 0,
-  background: "transparent",
-  position: "absolute",
+  background: 'transparent',
+  position: 'absolute',
   right: 20,
-  cursor: "pointer"
+  cursor: 'pointer'
 }
 
-export default ({ code, language = "jsx" }) => (
+export default ({ code, language = 'jsx' }) => (
   <Highlight
     {...defaultProps}
     theme={nightOwl}
