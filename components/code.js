8001eb23743bdacdce3d301e5c468e7f6020e15d
Sara Vieira <hey@iamsaravieira.com>
2018-09-23 19:26:48 +0200

    add copy to clipboard

--- components/code.js
+++ components/code.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import Highlight, { defaultProps } from 'prism-react-renderer'
-import nightOwl from 'prism-react-renderer/themes/nightOwl'
+import React from "react"
+import Highlight, { defaultProps } from "prism-react-renderer"
+import nightOwl from "prism-react-renderer/themes/nightOwl"
+import { CopyToClipboard } from "react-copy-to-clipboard"
+import { FaCopy } from "react-icons/fa"
 
 const styles = style => ({
   ...style,
-  padding: 20
+  padding: 20,
+  position: "relative"
 })
 
-export default ({ code, language = 'jsx' }) => (
+const buttonStyles = {
+  border: 0,
+  padding: 0,
+  background: "transparent",
+  position: "absolute",
+  right: 20,
+  cursor: "pointer"
+}
+
+export default ({ code, language = "jsx" }) => (
   <Highlight
     {...defaultProps}
     theme={nightOwl}
@@ -16,6 +28,11 @@ export default ({ code, language = 'jsx' }) => (
   >
     {({ className, style, tokens, getLineProps, getTokenProps }) => (
       <pre className={className} style={styles(style)}>
+        <CopyToClipboard text={code.trim()}>
+          <button style={buttonStyles}>
+            <FaCopy fill="#fff" />
+          </button>
+        </CopyToClipboard>
         {tokens.map((line, i) => (
           <div {...getLineProps({ line, key: i })}>
             {line.map((token, key) => (
